8ca2c5add269ca1be9d0e112450e2c5eb684e5d2
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-29 15:55:15 +0100

    feat: seconda funzione implementata

---
+++ js/modul/seconda-funzione.js
@@ -0,0 +1,12 @@
+console.log("ciao")
+
+
+
+const svaghi = (hobbyOne, hobbyTwo, hobbyThree) => {
+
+    const hobbies = [hobbyOne, hobbyTwo, hobbyThree];
+    const obgect = {hobbies};
+    return obgect
+}
+
+console.log(svaghi("playstation", "palestra", "cinema"));
