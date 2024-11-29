c346f79571a6e5840dd79e86b3bbcfb3eb4da90b
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-29 15:38:37 +0100

    feat: prima funzione scritta

---
+++ js/modul/prima-funzione.js
@@ -0,0 +1,20 @@
+
+console.log("ciao")
+
+// const uno = "francesco";
+// const due ="tullio"
+
+
+
+const identity = (name, surname) => {
+
+
+    const obgect = {firstName : name,
+            secondName : surname
+    }
+    return obgect
+}
+
+// console.log(result)
+
+console.log(identity("uno", "due"))
